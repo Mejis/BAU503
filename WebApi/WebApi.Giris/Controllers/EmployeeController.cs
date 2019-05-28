87da6bfae4d536e74f7a4c916ac0c461484aea69
Mejis <onuragici@slnyazilim.com>
2019-05-28 16:52:56 +0300

    dd
    dd

---
+++ WebApi/WebApi.Giris/Controllers/EmployeeController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApi.Giris.Models;
+
+namespace WebApi.Giris.Controllers
+{
+    public class EmployeeController : ApiController
+    {
+        static List<Employee> Employees = new List<Employee>()
+        {
+            new Employee(){Id=1,Name="Person 1"},
+            new Employee(){Id=2,Name="Person 2"},
+            new Employee(){Id=3,Name="Person 3"},
+        };
+
+        public IEnumerable<Employee> Get()
+        {
+            return Employees;
+        }
+
+        public Employee Get(int id)
+        {
+            return Employees.FirstOrDefault(x => x.Id == id);
+        }
+
+
+        //Attribute Based Routing
+        [Route("api/employee/{id}/tasks")]
+        public IEnumerable<string> GetEmployeeTask(int id)
+        {
+            switch (id)
+            {
+                case 1:
+                    return new List<string> { "Task 1-1", "Task 1-2", "Task 1-3" };
+                case 2:
+                    return new List<string> { "Task 2-1", "Task 2-2", "Task 2-3" };
+                case 3:
+                    return new List<string> { "Task 3-1", "Task 3-2", "Task 3-3" };
+                default:
+                    return null;
+            }
+        }
+    }
+}
---
+++ WebApi/WebApi.Giris/Controllers/ValuesController.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApi.Giris.Controllers
+{
+    public class ValuesController : ApiController
+    {
+
+        static List<string> degerler = new List<string>()
+        {
+            "value0","value1","value2"
+        };
+        //Convention Based Routing
+        // GET api/values
+        public IEnumerable<string> Get()
+        {
+            //return new string[] { "value1", "value2","value3" };
+            return degerler;
+        }
+
+        [HttpGet]
+        public IEnumerable<string> DegerGetir()
+        {
+            return degerler;
+        }
+
+        // GET api/values/5
+        public string Get(int id)
+        {
+            //return "value";
+            return degerler[id];
+        }
+        [HttpGet]
+        public string DegerGetir(int id)
+        {
+            return degerler[id];
+        }
+
+        [HttpPost]
+        public void DegerOlustur([FromBody]string value)
+        {
+           degerler.Add(value);
+        }
+        [HttpPut]
+        public void DegerGuncelle(int id, [FromBody]string value)
+        {
+            degerler[id] = value;
+        }
+
+        [HttpDelete]
+        public void DegerSil(int id )
+        {
+            degerler.RemoveAt(id);
+        }
+        // POST api/values
+        public void Post([FromBody]string value)
+        {
+            degerler.Add(value);
+        }
+
+
+
+        // PUT api/values/5
+        public void Put(int id, [FromBody]string value)
+        {
+            degerler[id] = value;
+        }
+
+        // DELETE api/values/5
+        public void Delete(int id)
+        {
+            degerler.RemoveAt(id);
+        }
+    }
+}
